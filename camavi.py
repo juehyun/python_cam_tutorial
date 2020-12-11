94e7f95cadbfd46e1a04781b67eda772ba91ab77
Joohyun Lee <juehyun@etri.re.kr>
2020-12-11 16:06:23 +0900

    minor changes

--- camavi.py
+++ camavi.py
@@ -1,15 +1,15 @@
 import cv2
 import datetime
 
-color = True
+color  = True
+width  = 1920
+height = 1080
 
-cap = cv2.VideoCapture(0)
-#cap.set(3,10000)
-#cap.set(4,10000)
-cap.set(3,1920)
-cap.set(4,1080)
-w = int(cap.get(3))
-h = int(cap.get(4))
+cam = cv2.VideoCapture(0) # 0 ~ 99
+cam.set(3,width)
+cam.set(4,height)
+w = int(cam.get(3))
+h = int(cam.get(4))
 print('width :%d, height : %d' % (w, h))
 print('quit  : q');
 print('color : c');
@@ -22,7 +22,7 @@ out          = cv2.VideoWriter(saveFileName, fourcc, 25.0, (w, h))
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 
 while(True):
-    ret, frame = cap.read()
+    ret, frame = cam.read()
     if(ret):
         if(color):
             cv2.imshow('frame', frame)
@@ -44,5 +44,30 @@ while(True):
             color = False
             print('set color mode : False')
 
-cap.release()
+cam.release()
 cv2.destroyAllWindows()
+
+# Microsoft LifeCam Studio
+# https://www.microsoft.com/accessories/en-us/business/lifecam-studio-for-business/5wh-00002#specsColumns-testCarousel
+# 1080p image and moving picture(avi) (even the spec said 720p moving picture and 1080p image)
+
+# cam.get(#)
+#  0. CV_CAP_PROP_POS_MSEC      Current position of the video file in milliseconds.
+#  1. CV_CAP_PROP_POS_FRAMES    0-based index of the frame to be decoded/captured next.
+#  2. CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file
+#  3. CV_CAP_PROP_FRAME_WIDTH   Width of the frames in the video stream.
+#  4. CV_CAP_PROP_FRAME_HEIGHT  Height of the frames in the video stream.
+#  5. CV_CAP_PROP_FPS           Frame rate.
+#  6. CV_CAP_PROP_FOURCC        4-character code of codec.
+#  7. CV_CAP_PROP_FRAME_COUNT   Number of frames in the video file.
+#  8. CV_CAP_PROP_FORMAT        Format of the Mat objects returned by retrieve() .
+#  9. CV_CAP_PROP_MODE          Backend-specific value indicating the current capture mode.
+# 10. CV_CAP_PROP_BRIGHTNESS    Brightness of the image (only for cameras).
+# 11. CV_CAP_PROP_CONTRAST      Contrast of the image (only for cameras).
+# 12. CV_CAP_PROP_SATURATION    Saturation of the image (only for cameras).
+# 13. CV_CAP_PROP_HUE           Hue of the image (only for cameras).
+# 14. CV_CAP_PROP_GAIN          Gain of the image (only for cameras).
+# 15. CV_CAP_PROP_EXPOSURE      Exposure (only for cameras).
+# 16. CV_CAP_PROP_CONVERT_RGB   Boolean flags indicating whether images should be converted to RGB.
+# 17. CV_CAP_PROP_WHITE_BALANCE Currently unsupported
+# 18. CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
